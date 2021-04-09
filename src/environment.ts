f889c758ac3295e58f6ab744225deb3c778bc745
Ralfs <lagzda7@inbox.lv>
2021-04-10 00:57:50 +0300

    package scripts update

--- src/environment.ts
+++ src/environment.ts
@@ -3,7 +3,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  dbConnectionString: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOSTS}/${process.env.DB}${process.env.DB_OPTIONS}`,
+  dbConnectionString: (process.env.NODE_ENV == 'prod') ?
+    `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOSTS}/${process.env.DB}${process.env.DB_OPTIONS}`:
+    'mongodb://localhost:27017',
   db: process.env.DB || '',
   port: process.env.PORT || 3000,
 
